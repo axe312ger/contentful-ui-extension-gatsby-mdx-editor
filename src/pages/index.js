a2992bf86fe02e73e13feb8713fc0216062b009e
Benedikt Rötsch <opensource@axe312.dev>
2020-08-14 15:27:29 +0200

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,20 @@ import MdxEditor from "../components/contentful/editor"
 
 const defaultMdx = `# Contentful UI MDX Editor Demo
 
-This is a **demo** to integrate an live MDX editor into Contentful.
+This is a **demo** to integrate a live MDX editor into Contentful.
 
 The \`<Columns />\` element is a very simple example component using flexbox to display content next to each other.
 
 <Columns>
 
-![](https://source.unsplash.com/random?1)
+![](https://source.unsplash.com/800x600/daily?sunset)
 
-![](https://source.unsplash.com/random?2)
+![](https://source.unsplash.com/800x600/daily?dogs)
 
-![](https://source.unsplash.com/random?3)
+![](https://source.unsplash.com/800x600/daily?cats)
 
 </Columns>
 
-## Feel free to play around!
-`
+## Feel free to play around!`
 
 export default () => <MdxEditor editorId="demo" initialValue={defaultMdx} />
